38a8bbf0bc24ea5659df2dfcd7684783e76baa43
liujing <2870640960@qq.com>
2023-04-28 16:51:51 +0800

    商品完成单个删除

--- src/views/modules/product/category.vue
+++ src/views/modules/product/category.vue
@@ -1,6 +1,29 @@
 <template>
   <div>
-    <el-tree :data="menus" :props="defaultProps" @node-click="handleNodeClick"></el-tree>
+    <el-tree :data="menus" :props="defaultProps"
+             :expand-on-click-node="false"
+             show-checkbox node-key="catId"
+             :default-expanded-keys="expandedKey">
+      <span class="custom-tree-node" slot-scope="{ node, data }">
+        <span>{{ node.label }}</span>
+        <span>
+          <el-button
+            v-if="node.level <= 2"
+            type="text"
+            size="mini"
+            @click="() => append(data)">
+            Append
+          </el-button>
+          <el-button
+            v-if="node.childNodes.length === 0"
+            type="text"
+            size="mini"
+            @click="() => remove(node, data)">
+            Delete
+          </el-button>
+        </span>
+      </span>
+    </el-tree>
   </div>
 </template>
 <script>
@@ -10,15 +33,16 @@ export default {
 //import 引入的组件需要注入到对象中才能使用
   components: {},
   props: {},
-  data() {
+  data () {
     //这里存放数据
     return {
       menus: [],
+      expandedKey: [],
       defaultProps: {
         children: 'children',
         label: 'name'
       }
-    };
+    }
   },
   //计算属性 类似于 data 概念
   computed: {},
@@ -26,48 +50,82 @@ export default {
   watch: {},
   //方法集合
   methods: {
-    handleNodeClick(data) {
-      console.log(data);
-    },
-    getMenus() {
+    getMenus () {
       this.$http({
         url: this.$http.adornUrl('/product/category/list/tree'),
         method: 'get'
       }).then(({data}) => {
-        console.log("成功获取到菜单数据...",data.data);
+        console.log('成功获取到菜单数据...', data.data)
         this.menus = data.data
       })
-    }
+    },
+
+    append (data) {
+      console.log('append', data)
+    },
+
+    remove (node, data) {
+      var ids = [data.catId]
+      this.$confirm(`是否删除【${data.name}】菜单?`, '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        this.$http({
+          url: this.$http.adornUrl('/product/category/delete'),
+          method: 'post',
+          data: this.$http.adornData(ids, false)
+        }).then(({data}) => {
+          this.$message({
+            message: '菜单删除成功',
+            type: 'success'
+          })
+          console.log('删除成功...', '当前id为' + ids, '父ID为' + node.data.parentCid)
+          //刷新出新的菜单
+          this.getMenus()
+          //设置需要默认展开的菜单
+          this.expandedKey = [node.data.parentCid]
+        })
+        console.log('remove', node, data)
+        console.log('父ID',node.data.parentCid)
+      }).catch(() => {
+        this.$message({
+          type: 'info',
+          message: '已取消删除'
+        })
+      })
+      console.log('remove', node, data)
+    },
   },
   //生命周期 - 创建完成（可以访问当前 this 实例）
-  created() {
-    this.getMenus();
+  created () {
+    this.getMenus()
   },
   //生命周期 - 挂载完成（可以访问 DOM 元素）,
-  mounted() {
+  mounted () {
   },
   //生命周期 - 创建之前
-  beforeCreate() {
+  beforeCreate () {
   },
   //生命周期 - 挂载之前
-  beforeMount() {
+  beforeMount () {
   },
   //生命周期 - 更新之前
-  beforeUpdate() {
+  beforeUpdate () {
   },
   //生命周期 - 更新之后
-  updated() {
+  updated () {
   },
   //生命周期 - 销毁之前
-  beforeDestroy() {
+  beforeDestroy () {
   },
   //生命周期 - 销毁完成
-  destroyed() {
+  destroyed () {
   },
   //如果页面有 keep-alive 缓存功能，这个函数会触发
-  activated() {
+  activated () {
   }
-};
+}
 </script>
 <style scoped>
 </style>
